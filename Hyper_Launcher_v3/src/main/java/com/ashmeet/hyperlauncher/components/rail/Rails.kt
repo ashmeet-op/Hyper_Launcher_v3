abd8228c788462c040b04da6ec65f2f137627238
ASHMEET <hollowlauncher@gmail.com>
2026-09-23 08:30:17 +0530

    Mercury_Phase_4

--- Hyper_Launcher_v3/src/main/java/com/ashmeet/hyperlauncher/components/rail/Rails.kt
+++ Hyper_Launcher_v3/src/main/java/com/ashmeet/hyperlauncher/components/rail/Rails.kt
@@ -14,10 +14,10 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
@@ -80,6 +80,7 @@ fun SideNavigationRail(
         windowInsets = WindowInsets(0.dp),
         modifier = Modifier
             .fillMaxHeight()
+            .width(110.dp)
     ) {
         Column(
             modifier = Modifier
